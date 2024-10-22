f3701c9140c96971a86ad2c7c54aa81c567f4ef1
Dom Davis <mrrobotcalendar@gmail.com>
2024-10-22 11:15:08 -0400

    sort drinking cues alphabetically

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,13 @@ import { PrismaClient } from "@prisma/client";
 async function seed() {
   const prisma = new PrismaClient();
   try {
-    await prisma.drinkingCue.update({
+    await prisma.event.update({
       where: {
-        id: "5b7cc13a-e09d-450b-9a5e-be7da9ab84fd",
+        id: "01fb665b-c20c-4d0e-9ec3-de37f4b5914d",
       },
       data: {
-        cue: "God/Deus",
+        notes:
+          "Darlene still wearing same outfit as the last episode, so I guess fsociety just stayed up all night at the arcade?",
       },
     });
   } catch (e) {
