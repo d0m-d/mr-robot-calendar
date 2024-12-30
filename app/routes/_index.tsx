c5237189f082d972cb23b6cb47003556084b02bd
Dom Davis <mrrobotcalendar@gmail.com>
2024-12-30 15:29:42 -0500

    fix image display in firefox

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,11 +11,12 @@ export default function Index() {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 confirm-dialog">
       <div className="px-4 min-h-screen md:flex md:items-center md:justify-center">
-        <div className="bg-black text-white rounded-lg md:max-w-fit md:mx-auto p-4 fixed inset-x-0 md:bottom-0 z-50 mb-4 mx-4 md:relative shadow-lg border-4 border-red-500 text-center items-center">
-          <img
-            src="/riskaheadposter2.jpg"
-            className="border-2 rounded-md border-black place-self-center mt-4"
-          />
+        <div className="bg-black text-white rounded-lg md:max-w-fit md:mx-auto p-4 inset-x-0 md:bottom-0 z-50 mb-4 md:relative shadow-lg border-4 border-red-500 text-center">
+          <div className="mt-4 flex justify-center">
+            {" "}
+            <img src="/riskaheadposter2.jpg" className="rounded-md" />
+          </div>
+
           <h1 className="text-4xl mt-4 font-semibold">CAUTION!</h1>
 
           <div className="text-xl font-medium p-2">
