0f5c8d5d92dae7e314d148a81110783cc5e784f0
Dom Davis <mrrobotcalendar@gmail.com>
2024-12-30 14:36:53 -0500

    remove unused code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,4 @@
-import { Link, MetaFunction, useLoaderData } from "@remix-run/react";
-import { useState } from "react";
-import { ScreeningQuestionType } from "~/helpers/types";
-import { db } from "~/utils/db.server";
+import { Link, MetaFunction } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,69 +7,21 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader = async () => {
-  const data = await db.screeningQuestions.findMany();
-  return data;
-};
-type LoaderType = Awaited<ReturnType<typeof loader>>;
-
 export default function Index() {
-  const data = useLoaderData<LoaderType>();
-  let correctAnswers = 0;
-  const [enterSiteEnabled, setEnterSiteEnabled] = useState(false);
-
-  const enableButton = () => {
-    if (correctAnswers === data.length) setEnterSiteEnabled(true);
-  };
-  const checkAnswer = (
-    question: ScreeningQuestionType,
-    answerInput: string
-  ) => {
-    if (
-      answerInput
-        .toLowerCase()
-        .replace(/[^a-zA-Z ]/g, "")
-        .trim() === question.answer.toLowerCase()
-    ) {
-      correctAnswers++;
-      return <p className="ml-2 mt-2 font-bold text-green-500">✓</p>;
-    } else return <p className="ml-2 mt-2 text-red-500">x</p>;
-  };
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 confirm-dialog">
       <div className="px-4 min-h-screen md:flex md:items-center md:justify-center">
         <div className="bg-black text-white rounded-lg md:max-w-fit md:mx-auto p-4 fixed inset-x-0 md:bottom-0 z-50 mb-4 mx-4 md:relative shadow-lg border-4 border-red-500 text-center items-center">
-          <h1 className="text-4xl mb-4 font-semibold">CAUTION!</h1>
           <img
             src="/app/img/riskaheadposter2.jpg"
-            className="border-2 rounded-md border-black place-self-center"
+            className="border-2 rounded-md border-black place-self-center mt-4"
           />
+          <h1 className="text-4xl mt-4 font-semibold">CAUTION!</h1>
+
           <div className="text-xl font-medium p-2">
             This website is full of spoilers. Please only proceed if you have
             watched the entire series!
           </div>
-          {/* {data.map((question) => {
-            const [answerValue, setAnswerValue] = useState("");
-            return (
-              <div className="flex justify-center" key={question.id}>
-                <div className="flex-col">
-                  <p className="text-xl mt-2 text-center">
-                    {question.question}
-                  </p>
-                  <div className="flex justify-center">
-                    <input
-                      placeholder="enter answer here"
-                      className="p-2 my-2 border-2 border-red-500 rounded-md text-center bg-black"
-                      value={answerValue}
-                      onChange={(e) => setAnswerValue(e.target.value)}
-                      onBlur={enableButton}
-                    />
-                    {answerValue !== "" && checkAnswer(question, answerValue)}
-                  </div>
-                </div>
-              </div>
-            );
-          })} */}
           <div className="flex justify-center mt-2">
             <Link to="/calendar">
               <button
