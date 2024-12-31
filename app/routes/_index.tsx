eadf5acfb193497c4c03b3457b8b4efa00383fe1
Dom Davis <mrrobotcalendar@gmail.com>
2024-12-31 16:01:15 -0500

    add s1 wallpaper theme

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 confirm-dialog">
+    <div className="fixed inset-0 flex items-center justify-center z-50 top-32 md:top-0 confirm-dialog">
       <div className="px-4 min-h-screen md:flex md:items-center md:justify-center">
         <div className="bg-black text-white rounded-lg md:max-w-fit md:mx-auto p-4 inset-x-0 md:bottom-0 z-50 mb-4 md:relative shadow-lg border-4 border-red-500 text-center">
           <div className="mt-4 flex justify-center">
