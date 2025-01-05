d11dcb85a21abd76f66ab5d2ccf4fe990c92288f
Dom Davis <mrrobotcalendar@gmail.com>
2025-01-05 12:29:23 -0500

    install new prisma update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,10 +3,13 @@ import { PrismaClient } from "@prisma/client";
 async function seed() {
   const prisma = new PrismaClient();
   try {
-    await prisma.drinkingCue.create({
+    await prisma.drinkingCue.update({
+      where: {
+        id: "58555b85-5b16-44f8-95b4-ac2549e7e4c2",
+      },
       data: {
-        cue: "Trump",
-        type: "reference",
+        notes:
+          "any drug/alcohol use that occurs solely in Elliot's head (i.e. in F world or when Mr. Robot drinks) does not count.",
       },
     });
   } catch (e) {
